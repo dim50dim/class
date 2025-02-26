7539ec6a88859a5374da9b6a52f08bbb486562e7
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-26 19:53:09 +0100

    add new file inheritance.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 let empty = {};
 console.log(empty.toString());
+
+let myProto = Object.getPrototypeOf(empty);
+console.log(myProto);
---
+++ inheritance.js
@@ -0,0 +1,5 @@
+const me = { 
+    talk() {
+        return 'Talking';
+    }
+}
